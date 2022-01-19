17aa3ab8142053ad5f1a05aa2bc2a9a46bbb2ac8
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2022-01-19 18:55:56 +0100

    increase and decrease

---
+++ inc_dec/dec.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+long long dec(long long); // external declaration
+
+int main()
+{
+    printf("dec(10) = %llx\n", dec(10));
+}
---
+++ inc_dec/inc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+long long inc(long long); // external declaration
+
+int main()
+{
+    printf("inc(10) = %llx\n", inc(10));
+}
