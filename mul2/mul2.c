682f770e66e271e5cc178b49357cc8c2423dedab
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2022-01-19 18:36:03 +0100

    mul functions

---
+++ mul2/mul2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int mul2(int); // external declaration
+
+int main()
+{
+    printf("mul2(6) = %d\n", mul2(6));
+}
---
+++ mul3/mul3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int mul3(int); // external declaration
+
+int main()
+{
+    printf("mul3(6) = %d\n", mul3(6));
+}
