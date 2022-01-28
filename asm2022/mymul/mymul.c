3f41820098fcb49a40b7988939f815def4fb59dc
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2022-01-28 18:19:00 +0100

    mymul seg

--- asm2022/mymul/mymul.c
+++ asm2022/mymul/mymul.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
-typedef struct {
-        unsigned long long high;
-            unsigned long long low;
+typedef struct
+{
+    unsigned long long high;
+    unsigned long long low;
 } resmul;
 
 void mymul(unsigned long long, unsigned long long, resmul *res);
 
-int main() {
+int main()
+{
     resmul res;
 
     mymul(3, 6, &res);
-    printf("mymul(3, 6); res.high=0x%llx, res.low=0x%llx\n", res.high, res.low);
-    //mymul(3, 6); res.high=0x0, res.low=0x12
+    printf("\t\t\tmymul(3, 6); res.high=0x%llx, res.low=0x%llx\n", res.high,
+           res.low);
+    printf("result should be:\tmymul(3, 6); res.high=0x0, res.low=0x12\n");
 
     mymul(0x1000000000000000, 0x4, &res);
-    printf("mymul(0x1000000000000000, 4); res.high=0x%llx, res.low=0x%llx\n", res.high, res.low);
-    //mymul(0x1000000000000000, 4); res.high=0x0, res.low=0x4000000000000000
+    printf("\n\t\t\tmymul(0x1000000000000000, 4); res.high=0x%llx, "
+           "res.low=0x%llx\n",
+           res.high, res.low);
+    printf("result should be:\tmymul(0x1000000000000000, 4); res.high=0x0, "
+           "res.low=0x4000000000000000\n");
 
     mymul(0x1000000000000000, 0x10, &res);
-    printf("mymul(0x1000000000000000, 0x10); res.high=0x%llx, res.low=0x%llx\n", res.high, res.low);
-    //mymul(0x1000000000000000, 0x10); res.high=0x1, res.low=0x0
+    printf("\n\t\t\tmymul(0x1000000000000000, 0x10); res.high=0x%llx, "
+           "res.low=0x%llx\n",
+           res.high, res.low);
+    printf("result should be:\tmymul(0x1000000000000000, 0x10); res.high=0x1, "
+           "res.low=0x0\n");
 
     return 0;
 }
-
-
-
