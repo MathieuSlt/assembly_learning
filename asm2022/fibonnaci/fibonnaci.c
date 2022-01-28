9b01d37f93c9f1f892bacb871368e8b7b4a6841e
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2022-01-28 15:46:03 +0100

    myadd and fibo done

--- asm2022/fibonnaci/fibonnaci.c
+++ asm2022/fibonnaci/fibonnaci.c
@@ -3,15 +3,15 @@
 
 long long fibonnaci(long long);
 
-int main() {
-
-    printf("fibonnaci(8)=%lli\n", fibonnaci(8)); // 21
-    printf("fibonnaci(-5)=%lli\n", fibonnaci(-5)); // -1
+int main()
+{
+    printf("fibonnaci(0)=%lli result should be 0\n", fibonnaci(0));
+    printf("fibonnaci(1)=%lli result should be 1\n", fibonnaci(1));
+    printf("fibonnaci(2)=%lli result should be 1\n", fibonnaci(2));
+    printf("fibonnaci(3)=%lli result should be 2\n", fibonnaci(3));
+    printf("fibonnaci(4)=%lli result should be 3\n", fibonnaci(4));
+    printf("fibonnaci(5)=%lli result should be 5\n", fibonnaci(5));
+    printf("fibonnaci(8)=%lli result should be 21\n", fibonnaci(8));
+    printf("fibonnaci(-5)=%lli result should be -1\n", fibonnaci(-5));
     return 0;
 }
-
-
-
-
-
-
--- asm2022/myadd/myadd.c
+++ asm2022/myadd/myadd.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
-
 unsigned long long myadd(unsigned long long, unsigned long long);
 
-
-int main() {
-
+int main()
+{
     printf("myadd(3, 6)=0x%llx\n", myadd(3, 6));
     printf("myadd(-1, 6)=0x%llx\n", myadd(-1, 6));
 }
-
-
-
-
