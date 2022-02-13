8e38be8ebc42fb13b2de16352e1324d9e6c49b61
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2022-02-13 17:28:04 +0100

    itoa ok

--- asm2022/myitoa/myitoa.c
+++ asm2022/myitoa/myitoa.c
@@ -6,7 +6,11 @@ int main()
 {
     printf("myitoa(\"5\")=%lli\n", myitoa("5")); // 5
     printf("myitoa(\"25\")=%lli\n", myitoa("25")); // 25
+    printf("myitoa(\"2925\")=%lli\n", myitoa("2925")); // 25
 
     printf("myitoa(\"-5\")=%lli\n", myitoa("-5")); // -5
     printf("myitoa(\"-18\")=%lli\n", myitoa("-18")); // -18
+    printf("myitoa(\"-1887\")=%lli\n", myitoa("-1887")); // -1887
+
+    printf("myitoa(\"0\")=%lli\n", myitoa("0")); // 0
 }
